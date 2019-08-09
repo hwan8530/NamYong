20e5716164c0580bd1ebb36f9ef153f5315a165d
hwan8530 <aass2356@gmail.com>
2019-08-09 16:50:43 +0900

    Crawling base

---
+++ Crawling.py
@@ -0,0 +1,25 @@
+# Crawling Interface
+
+#from abc import *
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+class Crawling() :
+    url = ""
+    html = ""
+    source = ""
+    soup = ""
+    def __init__(self, url):
+        self.url = url
+        self.html = self.openUrl(self.url)
+        self.source = self.getSource()
+        self.soup = self.getSoup(self.source)
+
+    def openUrl(self, url):
+        return urlopen(url)
+
+    def getSource(self):
+        return self.html.read()
+
+    def getSoup(self, source):
+        self.soup = BeautifulSoup(source,"html.parser")
