043985886ef935d72b0e575f4a97ce9193f31ea0
hwan8530 <aass2356@gmail.com>
2019-08-16 18:13:55 +0900

    Auction crawling updated. (chance deal)

--- Auction.py
+++ Auction.py
@@ -1,31 +1,52 @@
-from asynchat import async_chat
+from bs4 import BeautifulSoup
 
-from Crawling import *
+from Crawling import Crawling
+from Product_Info import Product_Info
+
 
 class Auction(Crawling) :
     def __init__(self, url):
         super().__init__(url)
 
     def getSoup(self, source):
-        return BeautifulSoup(source,"html.parser")
+        return BeautifulSoup(source, "html.parser")
 
     def setTable(self):
         return self.soup.find(id="section--inner_content_body_container")
 
     def setItems(self, table):
-        return table.find_all(class_="section--module_wrap")
+        return table.find_all(class_="component component--item_card type--general component--item_card_for_design")
 
     def printAllItems(self):
-        table = self.setTable();
-        items = self.setItems(table);
+        table = self.soup.find(id="section--inner_content_body_container")
+        items = table.find_all(class_="section--itemcard")
+        for item in items :
+            link = item.find("a").attrs['href']
+            a = item.find("a")
+            print(item)
+
+            #if image != None:
+             #   print(image.attrs['src'])
+
+            title = item.find(class_="text--title").getText()
+            price = item.find(class_="text--price_seller").getText()
+           # print(image)
+            #print("제품명 : " + title + " 가격 : " + price)
+            #print(" 링크 : " + link)
+
+    def printChanceItems(self):
+        table = self.soup.find(class_="component component--chance_shopping")
+        items = table.find_all(class_="item")
 
         for item in items :
-            if item != None:
-                info = item.find(class_="section--item_information")
-                title = info.find(class_="link--description_1").getText();
-                price = info.find(class_="text--price_number")
-                print(title+" "+price)
-
+            if item != None :
+             link = item.find("a").attrs['href']
+             title = item.find("img").attrs['alt']
+             image = item.find("img").attrs['src']
+             price = item.find(class_="text--price_number").getText()
+             print("제품명 : " + title + " 가격 : "+price)
+             print("image : "+image + " 링크 : "+link)
 
 ac = Auction("http://browse.auction.co.kr/search?keyword=%EB%A9%B4%EB%8F%84%EA%B8%B0&itemno=&nickname=&frm=hometab&dom=auction&isSuggestion=No&retry=&Fwk=%EB%A9%B4%EB%8F%84%EA%B8%B0&acode=SRP_SU_0100&arraycategory=&encKeyword=%EB%A9%B4%EB%8F%84%EA%B8%B0")
+ac.printChanceItems()
 ac.printAllItems()
