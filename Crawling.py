fe5a597b5993591ce0668c36c43346096617da31
hwan8530 <aass2356@gmail.com>
2019-08-18 13:44:59 +0900

    Update Crawling method

--- Crawling.py
+++ Crawling.py
@@ -12,12 +12,21 @@ class Crawling() :
     driver = ""
 
     def __init__(self, url):
-        self.option = webdriver.ChromeOptions()
-        self.option.add_argument('headless')
-        self.option.add_argument('window-size=1920x1080')
-        self.option.add_argument("disable-gpu")
-        self.driver = webdriver.Chrome('./chromedriver', chrome_options=self.option)
+        #self.option = webdriver.ChromeOptions()
+        #self.option.add_argument('headless')
+        #self.option.add_argument('window-size=1920x1080')
+        #self.option.add_argument("disable-gpu")
+        #self.driver = webdriver.Chrome('./chromedriver', chrome_options=self.option)
+        self.driver = webdriver.Chrome()
         self.driver.get(url)
-        self.html = self.driver.page_source
-        self.soup = BeautifulSoup(self.html, 'html.parser')
+
+    def waitForLoad(self):
+            self.driver.implicitly_wait(3)
+        # This method needs to override
+
+    def getHtml(self):
+            self.html = self.driver.page_source
+
+    def parseSoup(self):
+            self.soup = BeautifulSoup(self.html, 'html.parser')
 
